b923b60ce54ceee1ca1e559789017d3a3f2ef40a
hjmccain <hannahjmccain@gmail.com>
2017-03-16 13:14:07 -0400

    Tweaked landing page css.

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import headerShape from '../../public/small-screen-header.png';
-import headerShapeWithText from '../../public/cut-off-header.png';
+import headerShapeWithText from '../../public/new-header.png';
 
 const Header = () => {
   return (
@@ -9,6 +9,9 @@ const Header = () => {
         <img className="header-shape" src={headerShape} alt="blue shape" />
         <img className="header-shape-with-text" src={headerShapeWithText} alt="blue shape with intro text" />
         <div className="header-div">
+          <p className="header-title">
+            Hi there!
+          </p>
           <p className="header-text">
             I’m Hannah, a full-stack web developer based in New York City.</p>
         </div>
