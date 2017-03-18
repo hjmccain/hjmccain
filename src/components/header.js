50a4d6fc894360eff5c6e96bd780c995dab77892
hjmccain <hannahjmccain@gmail.com>
2017-03-18 18:34:37 -0400

    Scroll to anchors.

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import headerShapeWithText from '../../public/new-header.png';
 
 const Header = () => {
   return (
-    <div className="header-container">
+    <div className="header-container" id="header">
       <h1 className="intro-text-container">
         <img className="header-shape" src={headerShape} alt="blue shape" />
         <img className="header-shape-with-text" src={headerShapeWithText} alt="blue shape with intro text" />
