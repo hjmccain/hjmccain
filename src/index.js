1c067af5f75b7bb68674adfb24a88863a49f28c0
hjmccain <hannahjmccain@gmail.com>
2017-05-16 10:11:47 -0400

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IndexContainer from './components/index_container';
-import './index.css';
-
-ReactDOM.render(
-  <IndexContainer />,
-  document.getElementById('root')
-);
